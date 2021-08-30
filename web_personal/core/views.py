95a75c8195726082bdc034db772a3456c8a8f634
GuadalupeD <guadaluped@exo.com.ar>
2021-08-29 23:16:07 -0300

    Arreglo en las vistas y enlaces dentro del sitio. Nuevas imagenes e informacion

--- web_personal/core/views.py
+++ web_personal/core/views.py
@@ -6,8 +6,8 @@ def home(request):
 def about(request):
     return render(request, 'core/about.html')
 
-def portafolio(request):
-    return render(request, 'core/portafolio.html')
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
     return render(request, 'core/contact.html')
--- web_personal/web_personal/urls.py
+++ web_personal/web_personal/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('portafolio/', views.portafolio, name='portafolio'),
+    path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
